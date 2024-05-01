5b83865c5148aea393ca662d65e8cfe6b592fcf3
Sonu <Sonukr@users.noreply.github.com>
2024-05-01 17:29:34 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
-
+  
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
